ae571dbdbd4eb32d9775f37c93ca1f50c5664b8e
Paul Murray <44061707+Code4Wyatt@users.noreply.github.com>
2024-07-18 22:16:52 +0100

    Added PatientForm validation

--- patient-management-app/components/forms/PatientForm.tsx
+++ patient-management-app/components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -21,24 +22,28 @@ export enum FormFieldType {
     SKELETON = 'skeleton'
 }
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
-
 const PatientForm = () => {
     const [isLoading, setIsLoading] = useState(false)
-    
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            phone: "",
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true)
+
+        try {
+            const userData = { name, email, phone }
+
+            
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
